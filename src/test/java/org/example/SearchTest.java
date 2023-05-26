5b13cc038d325d0420f926b077aaf297df243900
Andrey Negrash <andi.ne2002@gmail.com>
2023-05-26 07:54:00 +0300

    Initial commit

---
+++ src/test/java/org/example/SearchTest.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
+public class SearchTest {
+    private static List<WebDriver> drivers;
+    private static final String request = "itmo";
+
+    @BeforeAll
+    public static void init() {
+        Helpers.setupDrivers();
+        drivers = Helpers.initDrivers();
+    }
+
+    @Test
+    public void testSignIn() {
+        openSiteToSearching();
+        enterInSearchLine();
+        clickSearchButton();
+    }
+
+    @AfterAll
+    public static void close() {
+        Helpers.closeDrivers(drivers);
+    }
+
+    public void openSiteToSearching() {
+        Helpers.openSite(drivers);
+        Helpers.timeout();
+    }
+
+    public void enterInSearchLine() {
+        String searchLineXpath = "/html/body/div/div[2]/ia-topnav/div[1]/primary-nav/nav/nav-search/div/form/input";
+        drivers.forEach(driver -> Helpers.findElement(driver, By.xpath(searchLineXpath)).sendKeys(request));
+        Helpers.timeout();
+    }
+
+    public void clickSearchButton() {
+        String xpath = "/html/body/div/div[2]/ia-topnav/div[1]/primary-nav/nav/nav-search/div/form/button";
+        drivers.forEach(driver -> Helpers.findElement(driver, By.xpath(xpath)).click());
+        Helpers.timeout();
+    }
+}
